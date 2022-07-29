9f82b07c9545855e9c299d34a564595598962ffa
unknown <andrija.ugrin77@gmail.com>
2022-07-29 19:19:38 +0200

    Vanilla JS Brooklyn players list

---
+++ stats.js
@@ -0,0 +1,218 @@
+/*0 znaci da su statsi zatvoreni, 1 da su otvoreni*/ 
+let toggleStatus = 0;
+
+function toggleStats1(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp1").style.display = "none";
+        document.querySelector(".infop1").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats1(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp1").style.display = "flex";
+        document.querySelector(".infop1").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats2(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp2").style.display = "none";
+        document.querySelector(".infop2").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats2(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp2").style.display = "flex";
+        document.querySelector(".infop2").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats3(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp3").style.display = "none";
+        document.querySelector(".infop3").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats3(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp3").style.display = "flex";
+        document.querySelector(".infop3").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats4(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp4").style.display = "none";
+        document.querySelector(".infop4").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats4(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp4").style.display = "flex";
+        document.querySelector(".infop4").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats5(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp5").style.display = "none";
+        document.querySelector(".infop5").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats5(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp5").style.display = "flex";
+        document.querySelector(".infop5").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats6(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp6").style.display = "none";
+        document.querySelector(".infop6").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats6(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp6").style.display = "flex";
+        document.querySelector(".infop6").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats7(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp7").style.display = "none";
+        document.querySelector(".infop7").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats7(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp7").style.display = "flex";
+        document.querySelector(".infop7").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats8(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp8").style.display = "none";
+        document.querySelector(".infop8").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats8(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp8").style.display = "flex";
+        document.querySelector(".infop8").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats9(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp9").style.display = "none";
+        document.querySelector(".infop9").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats9(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp9").style.display = "flex";
+        document.querySelector(".infop9").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats10(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp10").style.display = "none";
+        document.querySelector(".infop10").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats10(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp10").style.display = "flex";
+        document.querySelector(".infop10").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats11(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp11").style.display = "none";
+        document.querySelector(".infop11").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats11(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp11").style.display = "flex";
+        document.querySelector(".infop11").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
+
+function toggleStats12(){
+    if(toggleStatus == 0){
+        document.querySelector(".overlayp12").style.display = "none";
+        document.querySelector(".infop12").style.display = "unset";
+        toggleStatus = 1;
+        console.log("kokos");
+    }
+
+}
+function closeStats12(){
+    if(toggleStatus == 1){
+        document.querySelector(".overlayp12").style.display = "flex";
+        document.querySelector(".infop12").style.display = "none";
+        toggleStatus = 0;
+        console.log("bik");
+    }
+}
